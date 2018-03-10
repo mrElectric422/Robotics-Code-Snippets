a190ca790ad5ae4bc49a423e9636eb240f2130c3
mrElectric422 <37231124+mrElectric422@users.noreply.github.com>
2018-03-09 21:19:07 -0500

    Add files via upload

---
+++ TalonSR_potentiometer_board.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+Servo talonSR;
+
+int rawVal = 0;
+int mappedVal = 0;
+
+int potPin = 0;
+int talonPin = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+talonSR.attach(talonPin);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+rawVal = analogRead(potPin);
+mappedVal = map(rawVal, 0, 1023, 1000, 2000);
+
+Serial.println(mappedVal);
+
+talonSR.writeMicroseconds(mappedVal);
+
+delay(5);
+
+}
